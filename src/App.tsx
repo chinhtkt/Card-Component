3abe4198013406992fc1de7d3b4a570e27bdea39
chinhtkt <47823938+chinhtkt@users.noreply.github.com>
2021-04-06 21:33:14 +0700

    Not finished UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import CardIndex from './Components/CardIndex';
+import { CardDeck } from 'react-bootstrap';
+import { FoodList } from './Foodlist';
 
 function App() {
+  const list1: FoodList[] = [
+    {
+      name: '01 Seat',
+      flag: true,
+    },
+    {
+      name: 'Tea & coffee Breaks',
+      flag: false,
+    },
+    {
+      name: 'Wifi Available',
+      flag: false,
+    },
+    {
+      name: 'Exclusive Seatings',
+      flag: false,
+    }
+  ];
+
+  const list2: FoodList[] = [
+    {
+      name: '02 Seat',
+      flag: true,
+    },
+    {
+      name: 'Tea & Coffe Breaks',
+      flag: true,
+    },
+    {
+      name: 'Wifi Available',
+      flag: false,
+    },
+    {
+      name: 'Exclusive Seatings',
+      flag: false,
+    }
+  ]
+
+  const list3: FoodList[] = [
+    {
+      name: '04 Seat',
+      flag: true,
+    },
+    {
+      name: 'Tea & Coffe Breaks',
+      flag: true,
+    },
+    {
+      name: 'Wifi Available',
+      flag: true,
+    },
+    {
+      name: 'Exclusive Seatings',
+      flag: true,
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CardDeck className='cardfix'>
+        <div>
+          <CardIndex
+            text={'Basic'}
+            price={100}
+            desc={'Including all taxes'}
+            list={list1}
+          />
+        </div>
+        <div>
+          <CardIndex
+            text={'Standard'}
+            price={200}
+            desc={'Including all taxes'}
+            list={list2}
+          />
+        </div>
+        <div>
+          <CardIndex
+            text={'Premium'}
+            price={300}
+            desc={'Including all taxes'}
+            list={list3}
+          />
+        </div>
+      </CardDeck>
     </div>
   );
 }
